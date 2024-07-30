a69b7516b31fb535c42c920d41e25f317179a6cd
zlon <zlon@outlook.kr>
2024-07-30 18:22:04 +0900

    navigation

---
+++ lib/navigation.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'color_page1.dart';
+import 'color_page2.dart';
+import 'color_page3.dart';
+
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int _selectedIndex = 1; // 초기 인덱스를 1(Notifications)로 설정
+
+  final List<Widget> _pages = [
+    const Page1(),
+    const Page2(),
+    const Page3(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex], // 선택된 인덱스에 해당하는 페이지 표시
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        destinations: const <NavigationDestination>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.folder),
+            icon: Icon(Icons.folder),
+            label: 'page1',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'page2',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.settings),
+            icon: Icon(Icons.settings_outlined),
+            label: 'page3',
+          ),
+        ],
+      ),
+    );
+  }
+}
