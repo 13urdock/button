7906768b47e78d4f73c8c434914b9c7eee87a698
zlon <zlon@outlook.kr>
2024-07-30 18:33:44 +0900

    newpage

---
+++ lib/color_page4.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'color_page.dart';
+
+class Page4 extends ColorPage {
+  const Page4({Key? key})
+      : super(key: key, color: Colors.white, pageName: 'page4');
+}
