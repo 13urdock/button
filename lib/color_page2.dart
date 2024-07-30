2329a09cde437be0c72376e6ac60e38948f03d88
zlon <zlon@outlook.kr>
2024-07-30 18:22:04 +0900

    navigation

---
+++ lib/color_page2.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'color_page.dart';
+
+class Page2 extends ColorPage {
+  const Page2({Key? key})
+      : super(key: key, color: Colors.yellow, pageName: 'page2');
+}
