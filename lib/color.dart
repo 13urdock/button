ec0c2b51ea21d897ca64e42b8fc32f4f551ed0da
zlon <zlon@outlook.kr>
2024-07-31 00:50:00 +0900

    recolor

--- lib/color.dart
+++ lib/color.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const background = Color(0xFFFFD74A);
-  static const selectedColor = Color(0xFFFFB700);
-  static const sundayColor = Color(0xFFFF2A2A);
-  static const saturdayColor = Color(0xFF2043FF);
-  static const nomalText = Colors.black;
-  static const darkNomalText = Colors.white;
+  static const danchuYellow = Color(0xFFFFD74A);
+  static const deepYellow = Color(0xFFFFB700);
+  static const sundayred = Color(0xFFFF2A2A);
+  static const saturdayblue = Color(0xFF2043FF);
+  static const textColor = Colors.black;
+  static const white = Colors.white;
 }
