a5d272acee4d6d584c997c6b4e81a98a4ddc2af6
zlon <zlon@outlook.kr>
2024-07-31 03:49:55 +0900

    잠좀자자

--- lib/calendar/calendar_page.dart
+++ lib/calendar/calendar_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'calendar_style.dart';
 import 'package:danchu/color.dart';
+import 'schedule_page.dart';
 
+//캘린더 페이지
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
 
@@ -11,8 +13,8 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
-  DateTime _selectedDay = DateTime.now();
-  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now(); //초기날짜
+  DateTime _focusedDay = DateTime.now(); //선택날짜
 
   @override
   Widget build(BuildContext context) {
@@ -22,27 +24,49 @@ class _CalendarPageState extends State<CalendarPage> {
         title: Text('Calendar'),
         backgroundColor: AppColors.danchuYellow,
       ),
-      body: Container(
-        margin: const EdgeInsets.all(6.0),
-        child: TableCalendar(
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: _focusedDay,
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (
-            selectedDay,
-            focusedDay,
-          ) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-            });
-          },
-          calendarStyle: CalendarStyles.calendarStyle,
-          headerStyle: CalendarStyles.headerStyle,
-        ),
+      body: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(6.0),
+            child: TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+              calendarStyle: CalendarStyles.calendarStyle,
+              headerStyle: CalendarStyles.headerStyle,
+              calendarBuilders: CalendarStyles.calendarBuilders,
+            ),
+          ),
+          DraggableScrollableSheet(
+            initialChildSize: 0.1,
+            minChildSize: 0.1,
+            maxChildSize: 1.0,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: SchedulePage(
+                  selectedDay: _selectedDay,
+                  scrollController: scrollController,
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
