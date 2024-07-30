ca914a7e384f149a8b3a2c3b86884d9a13ce9db4
zlon <zlon@outlook.kr>
2024-07-31 00:38:11 +0900

    add color page

---
+++ lib/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const background = Color(0xFFFFD74A);
+  static const selectedColor = Color(0xFFFFB700);
+  static const sundayColor = Color(0xFFFF2A2A);
+  static const saturdayColor = Color(0xFF2043FF);
+  static const nomalText = Colors.black;
+  static const darkNomalText = Colors.white;
+}
