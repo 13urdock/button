92b4afe0e0a62c2e7b9880e048f2462bcbd7f006
JaeyeonE <ght010522@gmail.com>
2024-07-31 02:06:39 +0800

    final?

--- lib/calendar/calendar_page.dart
+++ lib/calendar/calendar_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'calendar_style.dart';
+import '../colorpallet.dart'; // 색상 팔레트 파일을 import 합니다.
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -12,6 +13,7 @@ class CalendarPage extends StatefulWidget {
 class _CalendarPageState extends State<CalendarPage> {
   DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
+  Color _selectedColor = Colors.blue; // 선택된 색상을 저장할 변수
 
   @override
   Widget build(BuildContext context) {
@@ -21,27 +23,44 @@ class _CalendarPageState extends State<CalendarPage> {
         title: Text('Calendar'),
         backgroundColor: CalendarStyles.background,
       ),
-      body: Container(
-        margin: const EdgeInsets.all(6.0),
-        child: TableCalendar(
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: _focusedDay,
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (
-            selectedDay,
-            focusedDay,
-          ) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-            });
-          },
-          calendarStyle: CalendarStyles.calendarStyle,
-          headerStyle: CalendarStyles.headerStyle,
-        ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(6.0),
+            child: TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+              calendarStyle: CalendarStyles.calendarStyle,
+              headerStyle: CalendarStyles.headerStyle,
+            ),
+          ),
+          ElevatedButton(
+            child: Text('Tap here'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: _selectedColor, // 버튼 색상을 선택된 색상으로 설정
+            ),
+            onPressed: () {
+              showColorPalette(
+                context,
+                (color) {
+                  setState(() {
+                    _selectedColor = color; // 선택된 색상 업데이트
+                  });
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
