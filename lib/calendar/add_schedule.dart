db5cda1f156cb7e64e4f953df1d58741705c2ce0
uyeong <uyeong7181@gmail.com>
2024-08-06 22:13:18 +0900

    modifying navigating from TodoList to calendar_page and adding design to add_schedule page

--- lib/calendar/add_schedule.dart
+++ lib/calendar/add_schedule.dart
@@ -26,19 +26,20 @@ class _AddSchedulePageState extends State<AddSchedulePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(
+            TextField( // 계획 제목
               controller: _titleController,
               decoration: InputDecoration(labelText: '제목'),
             ),
-            TextField(              controller: _descriptionController,
+            TextField( // 계획 설명
+              controller: _descriptionController,
               decoration: InputDecoration(labelText: '설명'),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
+            SizedBox(height: 20), // 아이콘 간 간격
+            ElevatedButton( // 날짜 선택 창(시간 선택으로 바꿔야함)
               child: Text(_selectedDate == null 
                 ? '날짜 선택' 
                 : '${_selectedDate!.year}/${_selectedDate!.month}/${_selectedDate!.day}'),
-              onPressed: () async {
+              onPressed: () async { // 날짜 선택 팝업 띄움
                 final pickedDate = await showDatePicker(
                   context: context,
                   initialDate: DateTime.now(),
@@ -53,14 +54,13 @@ class _AddSchedulePageState extends State<AddSchedulePage> {
               },
             ),
             SizedBox(height: 20),
-            Text('아이콘 선택'),
             GestureDetector(
               onTap: () {
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
                     return AlertDialog(
-                      title: Text('아이콘 선택'),
+                      title: Text('COLOR'),
                       content: IconSelector(
                         onColorSelected: (Color selectedColor) {
                           print('Selected color: $selectedColor');
@@ -73,23 +73,30 @@ class _AddSchedulePageState extends State<AddSchedulePage> {
               },
               child: CustomCircleIcon(
                 isSelected: true,  // 또는 상태에 따라 true/false
-                onPressed: () {
-                  showDialog(
+                onPressed: () async {  // 아이콘 색깔 선택창
+                  final Color? selectedColor = await showDialog<Color>( // Color? 타입으로 결과를 받습니다.
                     context: context,
                     builder: (BuildContext context) {
                       return AlertDialog(
-                        title: Text('아이콘 선택'),
+                        title: Text('COLOR'),
                         content: IconSelector(
-                          onColorSelected: (Color selectedColor) {
-                            print('Selected color: $selectedColor');
-                            // 여기에 상태 업데이트 로직을 추가할 수 있습니다.
+                          onColorSelected: (Color color) {
+                            Navigator.of(context).pop(color);  // 선택된 색상을 pop으로 반환
                           },
                         ),
                       );
                     },
                   );
-                },  // CustomCircleIcon 내부의 onPressed는 비워둡니다.
-                color: Colors.blue,  // 원하는 색상으로 설정
+
+                  if (selectedColor != null) {
+                    print('Selected color: $selectedColor');
+                    // 여기에서 선택된 색상으로 상태를 업데이트합니다.
+                    setState(() {
+                      // 예: _iconColor = selectedColor;
+                    });
+                  }
+                },
+                color: Colors.blue,  // 현재 선택된 색상 또는 기본 색상
                 size: 24,  // 원하는 크기로 설정
               ),
             ),
@@ -98,7 +105,7 @@ class _AddSchedulePageState extends State<AddSchedulePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.check),
+        child: Icon(Icons.check), // 스케줄 추가 완료 버튼, 입력된 데이터들은 schedule page에 표시됨
         onPressed: () {
           if (_titleController.text.isNotEmpty) {
             Navigator.of(context).pop(TodoItem(
