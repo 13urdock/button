6fa0c6edcd9e9c51318e26316c59466796d71ed1
zlon <zlon@outlook.kr>
2024-08-06 22:06:00 +0900

    calendar, draggable 파일 부활...

--- lib/calendar/calendar_page.dart
+++ lib/calendar/calendar_page.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:danchu/src/color.dart';
-import 'schedule_page.dart';
-import 'package:flutter/src/painting/edge_insets.dart';
+
 import '/src/color.dart';
-import '/src/draggable_style.dart';
-import 'schedule_list.dart';
-import '/src/calendar_style.dart';
-import 'add_schedule.dart';
+import 'calendar.dart';
+import 'calendar_draggable.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
@@ -20,32 +16,14 @@ class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.danchuYellow,
-      appBar: AppBar(
-        title: Text('Schedule Calendar'),
         backgroundColor: AppColors.danchuYellow,
-      ),
-      body: Stack(
-        children: [
+        appBar: AppBar(
+          title: Text('Danchu Calendar'),
+          backgroundColor: AppColors.danchuYellow,
+        ),
+        body: Stack(children: [
           Calendar(),
-          DraggableScrollable(
-            //darrable 기능 추가
-            child: Stack(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AddSchedulePage()),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+          CalendarDraggable(),
+        ]));
   }
 }
