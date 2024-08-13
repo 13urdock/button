ffc90c365624fb4ba7320f26d21226a3a1b73eef
zlon <zlon@outlook.kr>
2024-08-13 22:46:01 +0900

    danchu_page중간 업로드

--- lib/danchu/danchu_page.dart
+++ lib/danchu/danchu_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
-
-import '/src/color.dart';
+import '/src/draggable_style.dart';
 import 'danchu_calendar.dart';
-import 'danchu_draggable.dart';
+import 'package:danchu/diary/diary_list.dart';
+import '/src/color.dart';
+import '/diary/diary_entry.dart';
+import '/diary/diary_module.dart';
 
 class DanchuPage extends StatefulWidget {
   const DanchuPage({Key? key}) : super(key: key);
@@ -13,27 +14,70 @@ class DanchuPage extends StatefulWidget {
 }
 
 class _DanchuPageState extends State<DanchuPage> {
+  Map<DateTime, DiaryEntry> diaries = {};
   DateTime _selectedDay = DateTime.now();
 
-  void _onDaySelected(DateTime selectedDay) {
+  void _addOrUpdateEntry(DiaryEntry diaryEntry) {
     setState(() {
-      _selectedDay = selectedDay;
+      DateFunction.addOrUpdateEntry(diaries, diaryEntry);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    DiaryEntry? selectedDayEntry =
+        DateFunction.getSelectedDayEntry(diaries, _selectedDay);
+
     return Scaffold(
       backgroundColor: AppColors.danchuYellow,
       appBar: AppBar(
-        title: Text('Danchu Calendar'),
+        centerTitle: true,
         backgroundColor: AppColors.danchuYellow,
+        elevation: 0.0,
+        leading: InkWell(
+          onTap: () {},
+          child: Image.asset('assets/White_logo.png'),
+        ),
+        title: Text('Danchu'),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: InkWell(
+              onTap: () {},
+              child: Image.asset('assets/menu.png'),
+            ),
+          )
+        ],
       ),
       body: Stack(
         children: [
-          DanchuCalendar(onDaySelected: _onDaySelected),
-          SizedBox.expand(
-            child: DanchuDraggable(selectedDay: _selectedDay),
+          Column(
+            children: [
+              DanchuCalendar(
+                onDaySelected: (selectedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+          DraggableStyle(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                DiaryContentWidget(
+                  selectedDayEntry: selectedDayEntry,
+                  selectedDay: _selectedDay,
+                  onAddOrUpdateEntry: _addOrUpdateEntry,
+                ),
+                DiaryList(
+                  diaries: diaries.values.toList()
+                    ..sort((a, b) => b.date.compareTo(a.date)),
+                ),
+              ],
+            ),
           ),
         ],
       ),
